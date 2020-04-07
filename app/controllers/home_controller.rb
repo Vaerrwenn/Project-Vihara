7f11d95b035970bf76d1784bd678235b0df4180c
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2020-04-07 17:30:26 +0700

    Initial Commit
    Initial Commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+  end
+end
+
+# integrate Bootstrap 4 FA5 with RoR6
+# https://medium.com/swlh/integrate-bootstrap-4-and-font-awesome-5-in-rails-6-fec52ee51753
