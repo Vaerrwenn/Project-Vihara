3993e94b83492f39119bbe03ed0d6f93ad7e1b63
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2020-04-20 22:14:53 +0700

    Add "Add Deposit" feature (unfinished)

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,3 +1,7 @@
 class Deposit < ApplicationRecord
   belongs_to :member
+  validates :receipt_no, presence: true
+  validates :type, presence: true
+  validates :currency, presence: true
+  validates :value, presence: true
 end
