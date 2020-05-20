139d31205e4f1654c9ec5d42fdc72640e1681243
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2020-05-20 15:28:29 +0700

    Add account_number for members
    And some other fixes.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Vihara
     config.load_defaults 6.0
     config.time_zone = 'Jakarta'
     config.active_record.default_timezone = :local
-    config.force_ssl = true
+    # config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
