d8131d15973e24f40a934f99258a68f91f0da293
Albert Harican <11299005+Vaerrwenn@users.noreply.github.com>
2020-05-25 15:06:31 +0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Vihara
     config.load_defaults 6.0
     config.time_zone = 'Jakarta'
     config.active_record.default_timezone = :local
+    # ALWAYS COMMENT THIS ON DEVELOPMENT!!!
     config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
